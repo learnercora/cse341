146363ef77ce399d27b1b3ce6f458956b9d3afd4
coralin <zxc188950@yahoo.com.tw>
2025-05-07 14:39:23 +0800

    1-6

--- controllers/professional.js
+++ controllers/professional.js
@@ -1,7 +1,7 @@
 const mongodb = require('../db/connect');
 
 const getData = async (req, res, next) => {
-  const result = await mongodb.getDb().db('cse341').collection('user').find();
+  const result = await mongodb.getDb().db().collection('users').find();
   result.toArray().then((lists) => {
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(lists[0]); // we just need the first one (the only one)
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 const mongodb = require('./db/connect');
 const professionalRoutes = require('./routes/professional');
+const contactsRoutes = require('./routes/contacts');
 
 const port = process.env.PORT || 8080;
 const app = express();
@@ -17,7 +18,9 @@ app
     res.setHeader('Access-Control-Allow-Origin', '*');
     next();
   })
-  .use('/professional', professionalRoutes);
+  .use('/professional', professionalRoutes)
+  .use('/contacts', contactsRoutes);
+
 
 mongodb.initDb((err, mongodb) => {
   if (err) {
